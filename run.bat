43c70ddc8007c00a7a98d21945b5ed4ff1c8286e
Jontsgame <174447987+Jontsgame@users.noreply.github.com>
2026-02-16 16:24:26 +0100

    Update to run smoother
    Now it runs smoother because it deletes old files, and also it's asking now the user at the end if he want to start it now with QEMU or if he want to boot the .iso file later in VirtualBox

--- run.bat
+++ run.bat
@@ -1,22 +1,27 @@
 @echo off
-
 echo "It will now start to compile Assembly to an .img for QEMU and .iso for VirtualBox!"
 
 echo "Looking for old boot files and deleting them..."
+del boot.bin kernel.bin floppy.img
+del .\boot\floppy.img
+del .\boot\rusos-x86_64.iso
+del .\iso_root\floppy.img
 echo "Successfully deleted!"
 
-echo "Starting to compile Assemly..."
+echo "Starting to compile Assembly..."
 
 nasm -f bin boot.asm -o boot.bin
 nasm -f bin kernel.asm -o kernel.bin
+
+:: Falls du unter Windows kein 'sh' hast, musst du sicherstellen, dass dd im Pfad ist
 sh -c "dd if=/dev/zero of=floppy.img bs=512 count=2880"
 sh -c "dd if=boot.bin of=floppy.img conv=notrunc"
 sh -c "dd if=kernel.bin of=floppy.img bs=512 seek=1 conv=notrunc"
 
 echo "Finished compiling Assembly to floppy.img, it can now be used in QEMU!"
 
-mkdir iso_root
-mkdir boot
+mkdir iso_root 2>nul
+mkdir boot 2>nul
 copy floppy.img .\boot
 move /y floppy.img .\iso_root
 mkisofs -o rusos-x86_64.iso -b floppy.img -c boot.catalog iso_root
@@ -24,18 +29,15 @@ move rusos-x86_64.iso .\boot
 
 echo "Finished compiling floppy.img to rusos-x86_64.iso for usage in VirtualBox!"
 
-read -p "Do you want to start the OS now in QEMU, or later manually in VirtualBox? (now/later): " antwort
+:: --- HIER LIEGT DER FEHLER-FIX ---
+set /p antwort="Do you want to start the OS now in QEMU, or later manually in VirtualBox? (now/later): "
 
-if ["$antwort" = "now" then
+if /i "%antwort%"=="now" (
     echo "Starting in QEMU..."
-
     qemu-system-x86_64 -fda .\iso_root\floppy.img
-	
     echo "Started RusOS in QEMU!"
-else
+) else (
     echo "Understandable, have a great day! You will find the .iso in the 'boot' folder."
-fi
+)
 
 pause
-
-Oh... you ask why? So... I dont't know hehe :3 I just do it because Low-Level coding makes fun :D or maybe it is just what programmers doing when they are depressed... learning Assembly and making an OS in it... NAHHHH just kidding I am not depressed I am just having so much fun seeing how my own OS fully written in Assembly grows :D also shoutout to that dude called @gotssl on YouTube, you are the best, thanks! Even if you are just having 2 tutorials, you helped me a lot! So... YOU ARE IN THE CREDTS YAYYYY :P
